3adb83c78d2e4b556ba2254927c14153eae85d42
Sporsho <94753278+SARDEEN-RAHMAN-SPORSHO@users.noreply.github.com>
2026-01-18 18:02:56 +0600

    Create QuestionDetail.tsx

---
+++ src/pages/QuestionDetail.tsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        color: "black",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "18px",
+      }}
+    >
+      (File loaded)
+    </div>
+  );
+}
