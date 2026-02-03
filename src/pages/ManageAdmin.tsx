a9907e7b15b8ef6162f00540b51249487776e07e
Sporsho <94753278+SARDEEN-RAHMAN-SPORSHO@users.noreply.github.com>
2026-02-03 16:31:42 +0600

    Update ManageAdmin.tsx

--- src/pages/ManageAdmin.tsx
+++ src/pages/ManageAdmin.tsx
@@ -496,7 +496,7 @@ const ManageAdmin = () => {
                     </div>
                   </td>
                 </tr>
-              })}
+              ))}
             </tbody>
           </table>
         </div>
