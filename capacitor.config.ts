2afad3e99170b0f3504de8f4c602c85d2aec81ac
Md. Sardeen Rahman Sporsha <94753278+SARDEEN-RAHMAN-SPORSHO@users.noreply.github.com>
2026-04-18 00:42:49 +0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,7 +9,7 @@ const config: CapacitorConfig = {
   android: {
     allowMixedContent: false,
     captureInput: true,
-    webContentsDebuggingEnabled: false,
+    webContentsDebuggingEnabled: true,
   },
   plugins: {
     SplashScreen: {
